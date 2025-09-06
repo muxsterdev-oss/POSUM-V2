9e49cb4d2f2b1aa77f62af85212486f003a2afaf
muxsterdev-oss <muxsterdev@gmail.com>
2025-09-06 12:34:11 +0700

    Update POSUM.sol

--- src/mainnet/POSUM.sol
+++ src/mainnet/POSUM.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title POSUM Token
+ * @notice The official ERC20 token for the POSUM ecosystem.
+ */
+contract POSUM is ERC20Permit, Ownable {
+    uint256 public constant TOTAL_SUPPLY = 200_000_000 * 1e18;
+
+    constructor(address initialOwner)
+        ERC20("POSUM", "POSUM")
+        ERC20Permit("POSUM")
+        Ownable(initialOwner)
+    {
+        _mint(initialOwner, TOTAL_SUPPLY);
+    }
+}
 
